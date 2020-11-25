020361c739bddfc4b1194468fa1faaaaa73d6019
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-25 13:51:41 +0000

    car components added with getters and setters - to be tested at Car level

--- src/main/java/Components/Engine.java
+++ src/main/java/Components/Engine.java
@@ -1,4 +1,35 @@
 package Components;
 
 public class Engine {
+
+    private String size;
+    private String fuelType;
+
+
+    public Engine (String size, String fuelType){
+        this.size = size;
+        this.fuelType = fuelType;
+    }
+
+
+    public void setSize(String newSize) {
+        this.size = newSize;
+    }
+
+    public void setFuelType(String newFuelType) {
+        this.fuelType = newFuelType;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+
+
+
+
 }
--- src/main/java/Components/Tyres.java
+++ src/main/java/Components/Tyres.java
@@ -1,4 +1,21 @@
 package Components;
 
 public class Tyres {
+
+    private String type;
+    private int nrOfTyres;
+
+    public Tyres(String type, int nrOfTyres){
+        this.type = type;
+        this.nrOfTyres = nrOfTyres;
+
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void setType(String newType){
+        this.type = newType;
+    }
 }
